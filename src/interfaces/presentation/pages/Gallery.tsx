3ab983344e476c89f2621659e00d19bddeeb962a
Alexander Crespo <yunedor123@gmail.com>
2023-11-16 12:26:09 -0400

    restarting missing dependencies and errors.

--- src/interfaces/presentation/pages/Gallery.tsx
+++ src/interfaces/presentation/pages/Gallery.tsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Pagination, Navigation } from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
 
 import 'swiper/css'
 import 'swiper/css/navigation'
